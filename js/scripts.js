96531f3b68f525ee67ed51c7cf7727898d6b7d8e
Shradha Pulla and Tim Jung <shradha@>
2016-05-31 10:46:05 -0700

    created cooking quantities js script

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var number1 = parseInt(prompt("Enter quantity in oz to convert to cups"));
+var cups = (number1*0.125)
+alert(cups +" cups");
+var number2 = parseInt(prompt("Enter quantity in liter to convert to oz"));
+var oz = (number2*33.814)
+alert(oz + " oz");
