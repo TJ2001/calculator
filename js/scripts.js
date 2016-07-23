7abfc34019825c6a9b98a21c0e590da5fb36fcb5
TJ2001 <jung.m.tim@gmail.com>
2016-07-23 01:32:50 -0700

    updated javascript and fixed html

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
-var number1 = parseInt(prompt("Enter quantity in oz to convert to cups"));
-var cups = (number1*0.125)
-alert(cups +" cups");
-var number2 = parseInt(prompt("Enter quantity in liter to convert to oz"));
-var oz = (number2*33.814)
-alert(oz + " oz");
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+
+$("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
